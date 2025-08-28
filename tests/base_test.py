8ccccb46097c9502913e2275a141d81239d1819e
Anh Phuoc Le <63058749+anhplehub@users.noreply.github.com>
2025-08-28 14:16:56 +0700

    Modified tests file.

--- tests/base_test.py
+++ tests/base_test.py
@@ -17,8 +17,10 @@ class BaseTest(TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
         app.config['DEBUG'] = False
         app.config['PROPAGATE_EXCEPTIONS'] = True
-        with app.app_context():
-            db.init_app(app)
+        cls.app = app
+        cls.app_context = app.app_context
+        # with app.app_context():
+        #     db.init_app(app)
 
     def setUp(self):
         with app.app_context():
