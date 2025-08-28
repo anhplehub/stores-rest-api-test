1eff9d6e1817af378232f87acef817a24cdddf64
Anh Phuoc Le <63058749+anhplehub@users.noreply.github.com>
2025-08-28 11:20:36 +0700

    Fixed tests by include store idin every tests.

--- tests/unit/models/test_store.py
+++ tests/unit/models/test_store.py
@@ -3,7 +3,7 @@ from tests.unit.unit_base_test import UnitBaseTest
 
 class StoreTest(UnitBaseTest):
     def test_create_store(self):
-        store = StoreModel("test")
+        store = StoreModel("test ")
 
         self.assertEqual(store.name, "test",
                          "The name of the store after creation does not equal the constructor argument.")
